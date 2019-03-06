272333dbf8512982ed667e89cff07212470781c3
Andy Czerwonka <andy.czerwonka@gmail.com>
2019-03-06 07:56:59 -0700

    map & filter for expressions

--- src/test/scala/intro/TransformSpec.scala
+++ src/test/scala/intro/TransformSpec.scala
@@ -1,3 +1,34 @@
-package intro class TransformSpec {
+package intro
+import org.scalatest.{FunSuite, Matchers}
+
+class TransformSpec extends FunSuite with Matchers {
+
+  val range = 0 until 10
+  val even: Int => Boolean = (i: Int) => i % 2 == 0
+  val double: Int => Int = _ * 2
+
+  test("map & filter") {
+    val xs = range
+      .filter(even)
+      .map(double)
+    xs should contain inOrderOnly (0, 4, 8, 12, 16)
+  }
+
+  test("map & filter for expression sugar") {
+    val xs = for {
+      i <- range
+      if even(i)
+    } yield double(i)
+    xs should contain inOrderOnly (0, 4, 8, 12, 16)
+  }
+
+  test("some more point-free example") {
+    val xs = range
+      .filter(even)
+      .take(3)
+      .reverse
+      .map(double)
+    xs should contain inOrderOnly (8, 4, 0)
+  }
 
 }
